a9a792affc7961a9cbaea3eb3ba00f52fe55cda6
Vajresh <vajresh005@example.com>
2021-02-20 09:44:43 +0530

    showlist component now displays list of people form database in a table. Edit and delete actions need to updated. Till now we can add new entry which is validated and see the list of entries.

--- frontend/src/components/show-list-component.js
+++ frontend/src/components/show-list-component.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import '../css/form-style.css'
+import '../css/show-list.css';
+
+function DisplayList(props){
+    return (
+        <tr>
+            <td>{props.display.name}</td>
+            <td>{props.display.bloodGroup}</td>
+            <td>{new Date(props.display.donationDate).toDateString()}</td>
+            <td>Edit and Delete Buttons</td>
+        </tr>
+
+    );
+}
 
 export default class ShowList extends Component{
     constructor(props){
@@ -12,18 +24,36 @@ export default class ShowList extends Component{
     }
     componentDidMount(){
         axios.get('/donor/')
-            .then(donor => {
-                this.setState({
-                    donor: donor
-                })
-            })
+            .then(res => this.setState({ donor: res.data }))
             .catch(err => console.log(err));
+    }
+    showList(){
+        return (
+            this.state.donor
+                .map(arr => {
+                    console.log(arr.name);
+                    return <DisplayList display={arr} key={arr._id}/>;
+                })
+        );
     }
     render(){
         return (
-            <div>
+            <div className='container'>
                 <a href='/add'>Add New Entry</a>
-                <h1>Working{console.log(this.state.donor)}</h1>
+                <h1>Entries Added</h1>    
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Blood Group</th>
+                            <th>Recent Donation</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.showList()}
+                    </tbody>
+                </table>
             </div>
         )
     }
